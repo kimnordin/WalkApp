9ce0b94e1aa168c2169ccb3f332d8127b268f56f
Kim <nordinsane@gmail.com>
2024-06-17 20:31:34 +0200

    Initial Commit

---
+++ WalkApp/WalkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkAppApp.swift
+//  WalkApp
+//
+//  Created by Kim Nordin on 2024-06-17.
+//
+
+import SwiftUI
+
+@main
+struct WalkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
